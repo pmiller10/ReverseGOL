10366ea4ed0b8f2bca3c964110faaa20a4c5fe32
pmiller10 <pmiller987@gmail.com>
2013-11-20 04:59:22 +0000

    Cell class

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 class Board:
 
     def __init__(self, cells, height, width):
+        self.height, self.width = height, width
         total = height * width
         assert len(cells) == total
+        cells = [Cell(c, 0, 0, self) for c in cells]
         rows, columns = [], []
 
         # populate the rows
@@ -10,7 +12,9 @@ class Board:
         for h in range(height):
             row = []
             for w in range(width):
-                row.append(cells[pointer])
+                cell = cells[pointer]
+                cell.row = h
+                row.append(cell)
                 pointer += 1
             rows.append(row)
         self.rows = rows
@@ -19,6 +23,7 @@ class Board:
         start = 0
         for w in range(width):
             column = cells[start::width]
+            for c in column: c.column = w
             columns.append(column)
             start += 1
         self.columns = columns
@@ -28,6 +33,48 @@ class Board:
 
     def back(self):
         pass
+
+    def get(self, column, row):
+        return self.columns[column][row]
+
+    def _neighbors(self, column, row):
+        """
+        Return the 8 neighbors for a cell.
+        Takes as input the row and column
+        index for the given cell.
+        """
+        neighbor_columns = [column-1, column, column+1]
+        neighbor_rows = [row-1, row, row+1]
+        neighbors = []
+        for c in neighbor_columns:
+            for r in neighbor_rows:
+                # make sure it doesn't exceed the boundaries of the board
+                # and that it doesn't include the requested cell as a neighbor of itself
+                if c >= 0 and r >= 0 and c < self.width and r < self.height and not (c == column and r == row):
+                    cell = self.get(c,r)
+                    neighbors.append(cell)
+        return neighbors
+        
+
+
+class Cell:
+
+    def __init__(self, alive, column, row, board):
+        self.alive = alive
+        self.board = board
+        self.column = column
+        self.row = row
+
+    def neighbors(self):
+        return self.board._neighbors(self.column, self.row)
+
+    def __eq__(self, other):
+        return self.alive == other
+
+    def __repr__(self):
+        return str(self.alive)
+
+    
 
 if __name__ == "__main__":
     pass
--- tests.py
+++ tests.py
@@ -1,9 +1,10 @@
-from game import Board
+from game import Board, Cell
+
+cells = [1,1,1,0,0,0]
+height, width = 2,3
+board = Board(cells, height, width)
 
 def test_board():
-    cells = [1,1,1,0,0,0]
-    height, width = 2,3
-    board = Board(cells, height, width)
     assert board.columns[0] == [1,0]
     assert board.columns[1] == [1,0]
     assert board.columns[2] == [1,0]
@@ -11,5 +12,15 @@ def test_board():
     assert board.rows[1] == [0,0,0]
     print 'test_board pass'
 
+def test_cell():
+    living = Cell(1, None, 0, 0)
+    dead = Cell(0, None, 0, 0)
+    assert living.alive
+    assert not dead.alive
+    cell = board.get(1,1)
+    assert cell.neighbors() == [1, 0, 1, 1, 0]
+    print 'test_cell pass'
+
 if __name__ == "__main__":
     test_board()
+    test_cell()
